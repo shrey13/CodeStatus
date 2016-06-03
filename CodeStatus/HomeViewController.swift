f03cb091352ce7a0bfd473bcf5ee852421da0381
Shreyash Agrawal <shreyash@mit.edu>
2016-06-03 19:05:27 -0400

    Started iOS Project and Sketch File

---
+++ CodeStatus/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeStatus
+//
+//  Created by Shreyash Agrawal on 6/3/16.
+//  Copyright © 2016 codestatus. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CodeStatus/YesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  YesViewController.swift
+//  CodeStatus
+//
+//  Created by Shreyash Agrawal on 6/3/16.
+//  Copyright © 2016 codestatus. All rights reserved.
+//
+
+import UIKit
+
+class YesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
